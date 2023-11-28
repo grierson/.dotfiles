27e02c43fd5c6e2d86c03d043e4d767efc81610c
Kyle Grierson <griersonky@gmail.com>
2023-11-28 12:18:59 +0000

    view references

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -182,9 +182,8 @@ require("conform").setup({
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
 	lsp_zero.default_keymaps({ buffer = bufnr })
+	vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
 end)
 
 require('mason').setup({})
