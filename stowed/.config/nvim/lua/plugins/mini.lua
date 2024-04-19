9284e588739d503cfc1960d7a6ffa8ac5c44119f
Kyle Grierson <griersonky@gmail.com>
2024-04-19 17:19:55 +0100

    try to use mini.visits instead of harpoon

--- stowed/.config/nvim/lua/plugins/mini.lua
+++ stowed/.config/nvim/lua/plugins/mini.lua
@@ -13,12 +13,38 @@ return {
         },
     },
     config = function()
+        local extra = require('mini.extra')
+        local visits = require("mini.visits") -- Harpoon
+        visits.setup()
+
+        local getBranch = function()
+            local branch = vim.fn.system('git rev-parse --abbrev-ref HEAD')
+            if vim.v.shell_error ~= 0 then return nil end
+            return vim.trim(branch)
+        end
+
+        local markFile = function()
+            local path = getBranch()
+            visits.add_label(path)
+        end
+
+        local listMarks = function()
+            local path = getBranch()
+            visits.list_paths(nil, { filter = path })
+            extra.pickers.visit_paths(nil, {filter = path })
+        end
+
+        vim.keymap.set('n', '<Leader>M', markFile, { desc = "Add mark" })
+        vim.keymap.set('n', '<Leader>m', listMarks, { desc = "List marks" })
+        vim.keymap.set('n', '<Leader>x', function() visits.remove_label() end, { desc = "List marks" })
+
+        require('mini.pick').setup() -- Picker
         require('mini.completion').setup() -- Autocomplete
-        require('mini.basics').setup()   -- Better defaults
+        require('mini.basics').setup()     -- Better defaults
         require('mini.trailspace').setup() -- Trailing space
-        require('mini.comment').setup()  -- Comments - gcc
-        require('mini.pairs').setup()    -- Auto close
-        require('mini.surround').setup() -- add/change/delete surround
+        require('mini.comment').setup()    -- Comments - gcc
+        require('mini.pairs').setup()      -- Auto close
+        require('mini.surround').setup()   -- add/change/delete surround
         require('mini.cursorword').setup() -- Highlight current cursorword
         require('mini.jump2d').setup(
             {
@@ -26,12 +52,12 @@ return {
                     start_jumping = '',
                 },
             }
-        )                             -- Quick jump anywhere - <CR> <follow letters>
+        )                                 -- Quick jump anywhere - <CR> <follow letters>
         require('mini.bracketed').setup() -- Bracket movement
         require('mini.splitjoin').setup() -- gS split or join args
-        require('mini.starter').setup() -- Starter screen
-        require('mini.sessions').setup() -- Sessions
-        require('mini.notify').setup() -- Progress bar
+        require('mini.starter').setup()   -- Starter screen
+        require('mini.sessions').setup()  -- Sessions
+        require('mini.notify').setup()    -- Progress bar
         local hipatterns = require('mini.hipatterns')
         hipatterns.setup({
             highlighters = {
