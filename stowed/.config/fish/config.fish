b9561bc7ff38250984cc84ef40334bd789d64ef4
Kyle Grierson <griersonky@gmail.com>
2024-04-19 09:03:32 +0100

    fonts

--- stowed/.config/fish/config.fish
+++ stowed/.config/fish/config.fish
@@ -10,3 +10,15 @@ alias g="git"
 alias gg="g a .; and g c -a"
 alias lg="lazygit"
 alias n="nvim"
+
+if not set -q ZELLIJ
+    if test "$ZELLIJ_AUTO_ATTACH" = "true"
+        zellij attach -c
+    else
+        zellij
+    end
+
+    if test "$ZELLIJ_AUTO_EXIT" = "true"
+        kill $fish_pid
+    end
+end
