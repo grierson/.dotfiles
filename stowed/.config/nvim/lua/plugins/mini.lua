b6f227e1ef6eed6eea3f910510b88a66813c32ab
Kyle Grierson <griersonky@gmail.com>
2024-04-19 18:50:07 +0100

    harpoon -> mini.visits

--- stowed/.config/nvim/lua/plugins/mini.lua
+++ stowed/.config/nvim/lua/plugins/mini.lua
@@ -11,34 +11,38 @@ return {
             { "n", "v" },
             desc = "Jump 2d",
         },
+        {
+            '<Leader>M',
+            function()
+                local branch = vim.fn.system('git rev-parse --abbrev-ref HEAD')
+                if vim.v.shell_error ~= 0 then return nil end
+                local path = vim.trim(branch)
+                require("mini.visits").add_label(path)
+            end,
+            { "n", "v" },
+            desc = "Mark file"
+        },
+        {
+            '<Leader>m',
+            function()
+                local branch = vim.fn.system('git rev-parse --abbrev-ref HEAD')
+                if vim.v.shell_error ~= 0 then return nil end
+                local path = vim.trim(branch)
+                require("mini.extra")
+                    .pickers
+                    .visit_paths({
+                        filter = path,
+                        preserve_order = true
+                    })
+            end,
+            { "n", "v" },
+            desc = "Mark file"
+        }
     },
     config = function()
-        local extra = require('mini.extra')
-        local visits = require("mini.visits") -- Harpoon
-        visits.setup()
-
-        local getBranch = function()
-            local branch = vim.fn.system('git rev-parse --abbrev-ref HEAD')
-            if vim.v.shell_error ~= 0 then return nil end
-            return vim.trim(branch)
-        end
-
-        local markFile = function()
-            local path = getBranch()
-            visits.add_label(path)
-        end
-
-        local listMarks = function()
-            local path = getBranch()
-            visits.list_paths(nil, { filter = path })
-            extra.pickers.visit_paths(nil, {filter = path })
-        end
-
-        vim.keymap.set('n', '<Leader>M', markFile, { desc = "Add mark" })
-        vim.keymap.set('n', '<Leader>m', listMarks, { desc = "List marks" })
-        vim.keymap.set('n', '<Leader>x', function() visits.remove_label() end, { desc = "List marks" })
-
-        require('mini.pick').setup() -- Picker
+        require("mini.visits").setup()     -- Harpoon
+        require('mini.extra').setup()      -- Picker for harpon
+        require('mini.pick').setup()       -- Picker
         require('mini.completion').setup() -- Autocomplete
         require('mini.basics').setup()     -- Better defaults
         require('mini.trailspace').setup() -- Trailing space
--- stowed/.config/nvim/lua/plugins/theme.lua
+++ stowed/.config/nvim/lua/plugins/theme.lua
@@ -6,5 +6,6 @@ return {
         vim.cmd.colorscheme('alabaster')
         vim.o.termguicolors = true
         vim.o.background = "light"
+        vim.api.nvim_command("hi! CursorLine guifg=NONE guibg=" .. "#c9c9c9")
     end
 }
