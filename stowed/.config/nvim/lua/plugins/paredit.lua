df9fdba890de3b6eeaa416dd72ebc60c259ad5e2
Kyle Grierson <griersonky@gmail.com>
2024-04-26 16:55:38 +0100

    sexp

--- stowed/.config/nvim/lua/plugins/paredit.lua
+++ stowed/.config/nvim/lua/plugins/paredit.lua
@@ -3,5 +3,49 @@ return {
   ft = { "clojure", "scheme", "lisp", "racket", "fennel" },
   config = function()
     require("nvim-paredit").setup()
-  end
+  end,
+  keys = {
+    {
+      "<localleader>w",
+      function()
+        local paredit = require("nvim-paredit")
+        paredit.cursor.place_cursor(
+          paredit.wrap.wrap_element_under_cursor("( ", ")"),
+          { placement = "inner_start", mode = "insert" }
+        )
+      end,
+      desc = "Wrap element insert head"
+    },
+    {
+      "<localleader>W",
+      function()
+        local paredit = require("nvim-paredit")
+        paredit.cursor.place_cursor(
+          paredit.wrap.wrap_element_under_cursor("(", ")"),
+          { placement = "inner_end", mode = "insert" }
+        )
+      end,
+      "Wrap element insert tail",
+    },
+    { "<localleader>i",
+      function()
+        local paredit = require("nvim-paredit")
+        paredit.cursor.place_cursor(
+          paredit.wrap.wrap_enclosing_form_under_cursor("( ", ")"),
+          { placement = "inner_start", mode = "insert" }
+        )
+      end,
+      "Wrap form insert head",
+    },
+    { "<localleader>I",
+      function()
+        local paredit = require("nvim-paredit")
+        paredit.cursor.place_cursor(
+          paredit.wrap.wrap_enclosing_form_under_cursor("(", ")"),
+          { placement = "inner_end", mode = "insert" }
+        )
+      end,
+      "Wrap form insert tail",
+    }
+  }
 }
