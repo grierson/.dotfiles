1471ca96012c6546ea85ea4f1b35f5739bd88bd3
Kyle Grierson <griersonky@gmail.com>
2024-10-03 21:11:23 +0100

    fix alabaster LSP highlighting

--- stowed/.config/nvim/lua/plugins/lsp.lua
+++ stowed/.config/nvim/lua/plugins/lsp.lua
@@ -35,6 +35,13 @@ return {
         },
       })
 
+      -- Hide semantic highlights for functions
+      vim.api.nvim_set_hl(0, '@lsp.type.function', {})
+      -- Hide all semantic highlights
+      for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+        vim.api.nvim_set_hl(0, group, {})
+      end
+
       vim.api.nvim_create_autocmd('LspAttach', {
         desc = 'LSP actions',
         callback = function(event)
