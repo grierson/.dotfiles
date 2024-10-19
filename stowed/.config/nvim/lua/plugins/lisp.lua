d65005acfd72652f4825f70afdd024f3b432ce07
Kyle Grierson <griersonky@gmail.com>
2024-10-19 21:32:47 +0100

    wrap element

--- stowed/.config/nvim/lua/plugins/lisp.lua
+++ stowed/.config/nvim/lua/plugins/lisp.lua
@@ -14,6 +14,26 @@ return {
       ]])
     end,
     keys = {
+      { "<localleader>w",
+        function()
+          local paredit = require("nvim-paredit")
+          paredit.cursor.place_cursor(
+            paredit.wrap.wrap_element_under_cursor("( ", ")"),
+            { placement = "inner_start", mode = "insert" }
+          )
+        end,
+        "Wrap element insert head",
+      },
+      { "<localleader>W",
+        function()
+          local paredit = require("nvim-paredit")
+          paredit.cursor.place_cursor(
+            paredit.wrap.wrap_element_under_cursor("(", ")"),
+            { placement = "inner_end", mode = "insert" }
+          )
+        end,
+        "Wrap element insert tail",
+      },
       { "<localleader>i",
         function()
           local paredit = require("nvim-paredit")
